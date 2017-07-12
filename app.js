a65419380e7d1a1a65f9379a755cfd273a7ace24
seymar <seymar@users.noreply@github.com>
2017-07-12 17:06:43 +0200

    Converted to class to allow for multiple meter connections

--- app.js
+++ app.js
@@ -1,50 +1,54 @@
 /**
- * Tool for reading data from D-Link DSP-W215 Home Smart Plug.
- *
- * Usage: enter your PIN code to LOGIN_PWD, change value of HNAP_URL according to your device settings.
- *
- * @type {exports|module.exports}
+ * Dependencies
  */
-var soapclient = require('./js/soapclient');
-var fs = require('fs');
+var MQTTService = require('mqtt-service');
+var DSPW215 = require('dsp-w215-hnap');
 
-var OUTPUT_FILE = "result.txt";
-var LOGIN_USER = "admin";
-var LOGIN_PWD = "<PIN CODE>";
-var HNAP_URL = "http://192.168.1.128/HNAP1";
-var POLLING_INTERVAL = 60000;
-
-soapclient.login(LOGIN_USER, LOGIN_PWD, HNAP_URL).done(function (status) {
-    if (!status) {
-        throw "Login failed!";
+/**
+ * Config
+ */
+var config = {
+  meters: [
+    {
+      "url": "10.0.0.1",
+      "user": "admin",
+      "pincode": "123456",
+    },
+    {
+      "url": "10.0.0.2",
+      "user": "admin",
+      "pincode": "123456",
     }
-    if (status != "success") {
-        throw "Login failed!";
+  ],
+  pollInterval: 1000
+}
+
+//var dspw215s = [];
+
+config.meters.forEach(function(meter, i) {
+  var dspw215 = new DSPW215(meter.url);
+
+  // Login
+  dspw215.login(meter.user, meter.pincode).done(function(status) {
+    if(status == 'success') {
+      console.log('Connected to meter #' + i + ' (' + meter.url + ')');
+      
+      startPolling(dspw215);
+    } else {
+      throw new Error('No connection to meter #' + i + ' (' + meter.url + ')');
     }
-    start();
-});
+  });
 
-function start(){
-    soapclient.on().done(function (result){
-        console.log(result);
-        read();
-    })
-};
-
-function read() {
-    soapclient.consumption().done(function (power) {
-        soapclient.temperature().done(function (temperature) {
-            console.log(new Date().toLocaleString(), power, temperature);
-            save(power, temperature);
-            setTimeout(function () {
-                read();
-            }, POLLING_INTERVAL);
+  function startPolling(meterObj) {
+    // Start polling
+    setInterval(() => {
+      meterObj.consumption().done(function (power) {
+        meterObj.temperature().done(function (temperature) {
+          meterObj.totalConsumption().done(function (totalConsumption) {
+            console.log(new Date().toLocaleString(), power, temperature, totalConsumption);
+          });
         });
-    })
-}
-
-function save(power, temperature) {
-    fs.writeFile(OUTPUT_FILE, new Date().toLocaleString() + ";" + power + ";" + temperature + "\r\n", {flag: "a"}, function (err) {
-        if (err) throw err;
-    })
-}
+      })
+    }, config.pollInterval);
+  }
+});
